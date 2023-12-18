f5a90d1395254e4af55b9f2518d28ed085cd1866
powah999 <edwardwidjaja0507@gmail.com>
2023-12-18 22:30:44 +0700

    two projects added

---
+++ demos_and_projects/slider website/final/data.js
@@ -0,0 +1,28 @@
+const people = [
+  {
+    img: 'https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png',
+    name: 'Dimas',
+    job: 'Position: Fundraising',
+    text: `"Hi my name's Dimas"`,
+  },
+  {
+    img: 'https://www.japantimes.co.jp/uploads/imported_images/uploads/2022/06/np_file_164849.jpeg',
+    name: 'Alissa',
+    job: 'Position: Treasury',
+    text: `"I am treasured"`,
+  },
+  {
+    img: 'https://www.ucanews.com/uploads/2023/05/check-indonesians-fall-prey-to-fake-shaman-magic-money-scams-6467042a45c0a_600.jpeg',
+    name: 'Felicia',
+    job: 'Position: Design',
+    text: `"Wow saya suka design"`,
+  },
+  {
+    img: 'https://www.goodnewsfromindonesia.id/uploads/images/2021/01/2016332021-Pict.-Tersenyum.jpg',
+    name: 'Tommy',
+    job: 'Position: Secretary',
+    text: `"Saya secretary"`,
+  },
+];
+
+export default people;
